d07c7ae42feaef074d872aa4a9e7ae0da06e6aee
Guo Meng Tao <rinuo@qq.com>
2019-10-26 11:04:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,100 @@
 <?php
+/**
+ *
+ * Tree 构建tree状数据
+ *
+ * @author    guomengtao@gmail.com
+ * @version   v1.1.3
+ *
+ */
 
-echo "this is guomengtao";
+class Tree
+{
+    /**
+     * 主键名称.
+     *
+     * @var string
+     */
+    private static $primary = 'id';
 
-echo 666;
+    /**
+     * 父键名称.
+     *
+     * @var string
+     */
+    private static $parentId = 'parent_id';
+
+    /**
+     * 子节点名称.
+     *
+     * @var string
+     */
+    private static $child = 'child';
+
+    /**
+     * 修改主键名称、父键名称、子节点名称.
+     *
+     * @param string $primary
+     * @param string $parentId
+     * @param string $child
+     */
+    public static function setConfig($primary = '', $parentId = '', $child = '')
+    {
+        if (!empty($primary)) {
+            self::$primary = $primary;
+        }
+        if (!empty($parentId)) {
+            self::$parentId = $parentId;
+        }
+        if (!empty($child)) {
+            self::$child = $child;
+        }
+    }
+
+    /**
+     * 生成Tree.
+     *
+     * @param array  $data
+     * @param int $index
+     * @return array
+     */
+    public static function makeTree(&$data, $index = 0)
+    {
+        $children = self::findChild($data, $index);
+        if (empty($children)) {
+            return $children;
+        }
+        foreach ($children as $k => &$v) {
+            if (empty($data)) {
+                break;
+            }
+            $child = self::makeTree($data, $v[self::$primary]);
+            if (!empty($child)) {
+                $v[self::$child] = $child;
+            }
+        }
+        unset($v);
+
+        return $children;
+    }
+
+    /**
+     * 查找子类.
+     *
+     * @param array  $data
+     * @param number $index
+     * @return array
+     */
+    public static function findChild(&$data, $index)
+    {
+        $children = [];
+        foreach ($data as $k => $v) {
+            if ($v[self::$parentId] === $index) {
+                $children[] = $v;
+                unset($v);
+            }
+        }
+
+        return $children;
+    }
+}
