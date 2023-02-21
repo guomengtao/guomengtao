2f3d186c32555f89bf1a6efc232c35b7224500d6
Tom Jones <rinuo@qq.com>
2023-02-21 16:49:33 +0800

    Create mulltipass_use_note.mk

---
+++ mulltipass_use_note.mk
@@ -0,0 +1,26 @@
+# 下载
+
+## mac
+
+## Windows
+
+
+
+# 创建虚拟机命令
+
+## 显示所有已创建的虚拟机
+## 进入某台虚拟机
+
+
+
+# 查看Windows 本机IPconfig 命令
+
+# 安装composer命令 （同步安装了PHP？）
+
+# 安装 TP
+
+# 安装 Laravel
+
+# 安装 BaoTa
+
+
